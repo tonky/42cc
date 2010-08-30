0ad030f314e37032995a4551b6b971f1df597d96
Igor Tonky <gtonky@gmail.com>
2010-08-30 10:27:23 +0300

    ticket:1 Added selenium tests to check for bio on index page

---
+++ test/testWeb.py
@@ -0,0 +1,24 @@
+import unittest, time, re
+
+from selenium.remote import connect
+from selenium import FIREFOX
+
+class WebTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = connect(FIREFOX)
+        self.by_id = self.browser.find_element_by_id
+
+    def test_bio_index(self):
+        self.browser.get("http://localhost:8000/")
+        self.assertEquals(self.browser.get_title(), "My biography")
+        self.assertEquals(self.by_id("name").get_value(), "Igor")
+        self.assertEquals(self.by_id("surname").get_value(), "Tonky")
+        self.assertEquals(self.by_id("bio").get_value(), "Born and alive")
+        self.assertEquals(self.by_id("email").get_value(), "igor.tonky@gmail.com")
+
+    def tearDown(self):
+        self.browser.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
