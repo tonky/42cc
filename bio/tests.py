be5773c874df4d10c3b57d551a66a076d7a62fc0
Igor Tonky <gtonky@gmail.com>
2010-08-31 13:55:29 +0300

    ticket:3 changed tests from selenium to twill

--- bio/tests.py
+++ bio/tests.py
@@ -1,35 +1,15 @@
 import time, datetime
-
-from tddspry.django import HttpTestCase
-
-from selenium.remote import connect
-from selenium import FIREFOX
-
+from twill.commands import find, code, title, go
+from tddspry import NoseTestCase
 from bio.models import Log, Bio
 
-class WebTest(HttpTestCase):
+class WebTest(NoseTestCase):
     start_live_server = True
 
-    def setUp(self):
-        self.browser = connect(FIREFOX)
-        self.by_id = self.browser.find_element_by_id
-
-        a = Log.objects.all()
-        from django.db import connection
-
-    def test_bio_index(self):
-        self.browser.get("http://localhost:8000/")
-
-        self.assertEquals(self.browser.get_title(), "My biography")
-        self.assertEquals(self.by_id("name").get_text(), "Igor")
-        self.assertEquals(self.by_id("surname").get_text(), "Tonky")
-        self.assertEquals(self.by_id("bio").get_text(), "Born and alive")
-        self.assertEquals(self.by_id("email").get_text(), "igor.tonky@gmail.com")
-
     def test_middleware_logging(self):
-        self.browser.get("http://localhost:8000/test/me/")
-        last_two = Log.objects.order_by('-date')[:2] # by date
-        req = last_two[1] # firefox asks for favicon after the initial request
+        go("http://localhost:8000/test/me/")
+
+        req = Log.objects.order_by('-date')[0] # last by date
 
         now = datetime.datetime.now()
         self.assertEquals(req.method, "GET")
@@ -38,6 +18,3 @@ class WebTest(HttpTestCase):
         self.assertEquals(req.date.hour, now.hour)
         self.assertEquals(req.date.minute, now.minute)
         self.assertTrue((now.second - req.date.second) <= 1)
-
-    def tearDown(self):
-        self.browser.close()
