9aa8b8cff1132364019ab9cf0ffd3db3a2824257
Igor Tonky <gtonky@gmail.com>
2010-09-01 17:21:27 +0300

    ticket:1 fixed tests to use tddspry

--- bio/tests.py
+++ bio/tests.py
@@ -1,14 +1,13 @@
-from tddspry import NoseTestCase
-from twill.commands import find, code, title, go
+from tddspry.django.cases import HttpTestCase
 
 
-class WebTest(NoseTestCase):
+class WebTest(HttpTestCase):
     start_live_server = 1
 
     def test_bio_index(self):
-        go("http://localhost:8000/")
-        title("My biography")
-        find("Igor")
-        find("Tonky")
-        find("Born and alive")
-        find("igor.tonky@gmail.com")
+        self.go200("/")
+        self.title("My biography")
+        self.find("Igor")
+        self.find("Tonky")
+        self.find("Born and alive")
+        self.find("igor.tonky@gmail.com")
