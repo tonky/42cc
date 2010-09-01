e969c2062fae3ec7e534eeeec745661dabbe29b0
Igor Tonky <gtonky@gmail.com>
2010-09-01 09:42:10 +0300

    ticket:1 fixed tests autodiscovery and made code pass pylint and pep8 checks

--- bio/tests.py
+++ bio/tests.py
@@ -1,10 +1,10 @@
 from tddspry import NoseTestCase
-
 from twill.commands import find, code, title, go
+
 
 class WebTest(NoseTestCase):
     start_live_server = 1
-    
+
     def test_bio_index(self):
         go("http://localhost:8000/")
         title("My biography")
--- bio/views.py
+++ bio/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render_to_response
 from bio.models import Bio
 
+
 def index(request):
     bio = Bio.objects.get(pk=1)
     return render_to_response('index.html', {'bio': bio})
