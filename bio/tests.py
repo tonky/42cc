65b52437ee7004a9920cd8c1847948a3973b7c3a
Igor Tonky <gtonky@gmail.com>
2010-09-02 07:41:02 +0300

    ticket:4 added missing tests from #1 and #3

--- bio/tests.py
+++ bio/tests.py
@@ -1,10 +1,33 @@
+import datetime
 from tddspry.django import HttpTestCase
 import settings
 from django.test.client import Client
+from middleware.logging import Log
 
 
 class WebTest(HttpTestCase):
     start_live_server = True
+
+    def test_bio_index(self):
+        self.go200("/")
+        self.title("My biography")
+        self.find("Igor")
+        self.find("Tonky")
+        self.find("Born and alive")
+        self.find("igor.tonky@gmail.com")
+
+    def test_middleware_logging(self):
+        self.go("/test/me/")
+
+        req = Log.objects.order_by('-date')[0] # last by date
+
+        now = datetime.datetime.now()
+        self.assertEquals(req.method, "GET")
+        self.assertEquals(req.url, "/test/me/")
+        self.assertEquals(req.date.date(), now.date())
+        self.assertEquals(req.date.hour, now.hour)
+        self.assertEquals(req.date.minute, now.minute)
+        self.assertTrue((now.second - req.date.second) <= 1)
 
     def test_context_settings(self):
         c = Client()
