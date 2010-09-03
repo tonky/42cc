b57a11fdbc5d79d8f7fb9022b5f0e2c1e6414437
Igor Tonky <gtonky@gmail.com>
2010-09-03 19:48:13 +0300

    ticket:12 added an object_list generic view for first 10 middleware request logs

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from settings import STATIC_ROOT
+from bio.models import Log
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,6 +14,11 @@ urlpatterns = patterns('',
     (r'^save_ajax/$', 'bio.views.save_ajax'),
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^logout/$', 'bio.views.logoff'),
+    (r'^first10/$', 'django.views.generic.list_detail.object_list', 
+        {'template_name': 'ten_requests.html', 
+            'queryset'  : Log.objects.all()[:10]
+        }
+    ),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
