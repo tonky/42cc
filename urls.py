31cb99bd1efaeeaa5eae503e61103c2fbcf4817a
Igor Tonky <gtonky@gmail.com>
2010-09-03 19:50:20 +0300

    ticket:12 fixed pep8 errors

--- urls.py
+++ urls.py
@@ -14,11 +14,9 @@ urlpatterns = patterns('',
     (r'^save_ajax/$', 'bio.views.save_ajax'),
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^logout/$', 'bio.views.logoff'),
-    (r'^first10/$', 'django.views.generic.list_detail.object_list', 
-        {'template_name': 'ten_requests.html', 
-            'queryset'  : Log.objects.all()[:10]
-        }
-    ),
+    (r'^first10/$', 'django.views.generic.list_detail.object_list',
+        {'template_name': 'ten_requests.html',
+            'queryset': Log.objects.all()[:10]}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
