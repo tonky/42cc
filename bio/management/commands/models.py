0b0420f633f8f8f1bfa788bccbcc33101b7bf623
Igor Tonky <gtonky@gmail.com>
2010-09-03 12:45:46 +0300

    ticket:9 reworded and commented a bit

--- bio/management/commands/models.py
+++ bio/management/commands/models.py
@@ -9,34 +9,39 @@ class Command(NoArgsCommand):
     help = 'Prints all the models in project with their instances count in db'
 
     def handle(self, *args, **options):
-        project_apps = []
+        models_modules = []
         project_models = []
 
+        # filter apps, that are imported from site-packages
         for installed_app in INSTALLED_APPS:
             module = __import__(installed_app)
 
             if re.search("site-packages", module.__file__):
                 continue
 
+            # filter apps that don't have models
             if not "models" in module.models.__dict__:
                 continue
 
+            # filter models that don't provide "get_apps" functions
             if not "get_apps" in module.models.models.__dict__:
                 continue
 
-            module_apps = module.models.models.get_apps()
+            # get list of models for current 'models' module
+            models = module.models.models.get_apps()
 
-            for app in module_apps:
-                if re.search("site-packages", app.__file__):
+            # filter out models from python install
+            for model in models:
+                if re.search("site-packages", model.__file__):
                     continue
 
-                project_apps.append(app)
+                models_modules.append(model)
 
-
-        for app in project_apps:
-            for prop in app.__dict__:
-                if type(app.__dict__[prop]) == ModelBase:
-                    project_models.append(app.__dict__[prop])
+        # get only models classes, that are based on ModelBase
+        for module in models_modules:
+            for prop in module.__dict__:
+                if type(module.__dict__[prop]) == ModelBase:
+                    project_models.append(module.__dict__[prop])
 
         for m in project_models:
             print "%s: %d" % (m.__name__, m.objects.count())
